445be5c602889bdedacb232d819671d8de6d2722
Harsh <harsh20221@gmail.com>
2024-07-08 03:12:09 +0530

    Shopping List IOS

---
+++ lib/models/grocery_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class GroceryItem  {
+const GroceryItem({
+required this.id,required
+ this.category, required
+this.quantity, required
+this.name
+});
+final String id;
+final String name;
+final int   quantity;
+final Category category;
+
+}
