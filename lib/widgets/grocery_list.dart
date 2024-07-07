e713cbeb45ef9a2fd91097ad6042cc0de43233fa
Harsh <harsh20221@gmail.com>
2024-07-08 04:36:42 +0530

    More Changes

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 ////** Here We are Defining our Widget Class Grocery List*/
 
-
-class  GroceryList  extends StatelessWidget {
+class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
   @override
   Widget build(BuildContext context) {
-  return Text("My love");
+    return Scaffold(
+      appBar: AppBar(title: Text("Your Groceriers")),
+      body: ListView.builder(  //? ListView.builder is used to create a scrollable list of items
+          itemBuilder: (ctx, index) => ListTile( ///? ListTile is a single fixed-height row that typically contains some text as well as a leading or trailing icon.
+                title: Text(groceryItems[index].name), //? Text is a widget that displays a string of text with a single style.
+             leading: Container(width: 24,height: 24,color: groceryItems[index].category.color,), ),itemCount: groceryItems.length,),//? Here leading will display a little Category Icon for all the Categories 
+    );
   }
-  
 }
