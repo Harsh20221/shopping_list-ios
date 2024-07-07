24270df03929bb05e0afa5ce7ab53c8a60dad0ac
Harsh <harsh20221@gmail.com>
2024-07-08 05:26:15 +0530

    Added Button to go to Next screen -- new item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
 ////** Here We are Defining our Widget Class Grocery List*/
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget { ///# TIP- IF you want to change a stateless to a Stateful Widget then click on that small Bulb icon 
   const GroceryList({super.key});
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem(){
+  Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>const NewItem()));
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Your Groceriers")),
+      appBar: AppBar(title: Text("Your Groceriers"),actions: [IconButton(onPressed: (){}, icon:const Icon(Icons.shopping_basket))],),
       body: ListView.builder(
         //? ListView.builder is used to create a scrollable list of items
         itemBuilder: (ctx, index) => ListTile(
