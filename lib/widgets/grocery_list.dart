2431bacd61e8e10876faa76e2bfe1e9af7f9cc35
Harsh <harsh20221@gmail.com>
2024-07-09 06:16:33 +0530

    Comments and final Beta

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,11 +15,18 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   final List<GroceryItem> _groceryitems =
       []; //!!! Make sure to store the grocery items entered by user in this list
+
+  void _removeitem(GroceryItem item) {
+    ///*** This Void Function wll help us dismiss list items throug swipe  */
+    _groceryitems.remove(item);
+  }
+
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>( //!! Here This line newItem=await Navigator..... 
-    ///!!will be responsible for navigating to next screen and 
-    ///! will also hold the data  Entered in the next screen 
-        MaterialPageRoute(builder: (ctx) => const NewItem()));
+    final newItem = await Navigator.of(context)
+        .push<GroceryItem>(//!! Here This line newItem=await Navigator.....
+            ///!!will be responsible for navigating to next screen and
+            ///! will also hold the data  Entered in the next screen
+            MaterialPageRoute(builder: (ctx) => const NewItem()));
     if (newItem == null) {
       return;
     }
@@ -44,19 +51,20 @@ class _GroceryListState extends State<GroceryList> {
       ),
       body: ListView.builder(
         //? ListView.builder is used to create a scrollable list of items
-        itemBuilder: (ctx, index) => ListTile(
-
-            ///? ListTile is a single fixed-height row that typically contains some text as well as a leading or trailing icon.
-            title: Text(_groceryitems[index]
-                .name), //? Text is a widget that displays a string of text with a single style.
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: _groceryitems[index].category.color,
-            ),
-            trailing: Text(_groceryitems[index]
-                .quantity
-                .toString())), //!!Make sure to Manually convert this into string
+        itemBuilder: (ctx, index) => Dismissible(  //!! Make sure to enclose the entire function inside Dismissible 
+          key: ValueKey(_groceryitems[index].id), //! Do not forget to assign key , this key will be used to track the item
+          onDismissed: (direction) => _removeitem, //? definded in line 19 this function helps us remove list items on swiping the list item right or left 
+          child: ListTile(
+              ///? ListTile is a single fixed-height row that typically contains some text as well as a leading or trailing icon.
+              title: Text(_groceryitems[index]
+                  .name), //? Text is a widget that displays a string of text with a single style.
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryitems[index].category.color,
+              ),
+              trailing: Text(_groceryitems[index].quantity.toString())),
+        ), //!!Make sure to Manually convert this into string
         itemCount: _groceryitems.length,
       ), //? Here leading will display a little Category Icon for all the Categories
     );
